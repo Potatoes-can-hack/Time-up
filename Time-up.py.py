2b81df4c2809891fbd94748deb2613063b58d657
Potatoes <102719655+Potatoes-can-hack@users.noreply.github.com>
2022-09-03 14:03:05 +0530

    Add files via upload

---
+++ Time-up.py.py
@@ -0,0 +1,24 @@
+import time
+import os
+
+def shutdown():
+        return os.system('shutdown /s /t 1')
+
+# define the countdown func.
+def countdown(t):
+
+    while t:
+        mins, secs = divmod(t, 60)
+        timer = '{:02d}:{:02d}'.format(mins, secs)
+        print(timer, end="\r")
+        time.sleep(1)
+        t -= 1
+
+    print('Fire in the hole!!')
+    shutdown()
+
+# input time in seconds
+t = input("Enter the time in seconds: ")
+
+# function call
+countdown(int(t))
